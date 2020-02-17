2159d2da13db295e105300ac1b70263d480247fa
wing324 <yu.min@sjsu.edu>
2020-02-16 21:20:50 -0800

    add file

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -7,9 +7,9 @@ module.exports.loggedIn = function(req, res, next)
     {
         // Proceed if the user is logged in.
         console.log("Logged in: "); console.log(req.session.user);
-        next();  
-    } 
-    else 
+        next();
+    }
+    else
     {
         console.log("Not logged in");
         res.send("You must first log in.");
@@ -19,7 +19,7 @@ module.exports.loggedIn = function(req, res, next)
 /*
  * GET home page.
  */
-module.exports.index = function(req, res, next) 
+module.exports.index = function(req, res, next)
 {
     res.render('index', { title: 'Cornavirus Database Virtulization' });
    // console.log('Cookies: ', req.cookies);
@@ -28,23 +28,22 @@ module.exports.index = function(req, res, next)
 /*
  * GET registration page.
  */
-///module.exports.get_register = function(req, res)
-//{
-//    res.render('register',
-//               { message: "Please register!" });
-//};
+module.exports.get_register = function(req, res)
+{
+   res.render('register',
+              { message: "Please register!" });
+};
 
 /*
  * POST registration page.
  */
 module.exports.post_register = function(req, res)
 {
-	
     if(!req.body.username)
     {
         res.status("400");
         res.send("Username can not be empty!");
-    } 
+    }
     else if (!req.body.password) {
     	res.status("400");
     	res.send("Password can not be empty!");
@@ -55,17 +54,17 @@ module.exports.post_register = function(req, res)
                       {
                           return user.username === req.body.username;
                       });
-        
+
         // If there is a match, the user has already registered.
         if (matches.length > 0)
         {
             res.render('register', {message: "User already registered!"});
         }
-        
+
         // Register a new user.
         else
         {
-            var newUser = { username: req.body.username, 
+            var newUser = { username: req.body.username,
                             password: req.body.password };
             registeredUsers.push(newUser);
             console.log("New user:"); console.log(newUser);
@@ -90,16 +89,16 @@ module.exports.post_login = function(req, res)
 {
     console.log("Registered users:"); console.log(registeredUsers);
     console.log("Logging in: " + req.body.username + "/" + req.body.password);
-    
+
     // Create an array of users with matching credentials.
     var matches = registeredUsers.filter(function(user)
                   {
-                      return    (user.username === req.body.username) 
+                      return    (user.username === req.body.username)
                              && (user.password === req.body.password);
                   });
-    
+
     console.log("Matching credentials: "); console.log(matches);
-    
+
     if (matches.length === 0)
     {
         res.render('login', {message: "Invalid credentials!"});
@@ -108,10 +107,10 @@ module.exports.post_login = function(req, res)
     {
         // The user is logged in for this session.
         req.session.user = matches[0];
-        console.log("Sucessfully logged in:"); 
+        console.log("Sucessfully logged in:");
         console.log(req.session.user.username);
-        
-        res.render('loggedin', 
+
+        res.render('loggedin',
                    { name: req.session.user.username });
     }
 };
@@ -122,17 +121,17 @@ module.exports.post_login = function(req, res)
 module.exports.get_logout = function(req, res)
 {
     console.log("Logging out:");
-    
+
     if (req.session.user)
     {
         var name = req.session.user.username;
         console.log(name);
-        
+
         req.session.destroy(function()
         {
             console.log(name + " logged out.");
         });
-        
+
         res.send(name + " is now logged out.");
     }
     else
