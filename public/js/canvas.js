e6a84c597e0100bf4dedcfd86bdbdab782f95226
wing324 <yu.min@sjsu.edu>
2020-03-05 22:12:23 -0800

    add dropped object

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -6,8 +6,24 @@ window.onload = function() {
   context.fillText("PRAY FOR WUHAN!", 25, 45);
 };
 
-// Drag Me
+// Drag and Drop Me
 $(init);
 function init() {
   $("#topPay").draggable();
+  $("#target").droppable();
+
+  $("#target").bind("drop", highlightTarget);
+  $("#target").bind("dropout", reset);
 }
+
+function highlightTarget(event, ui) {
+  $("#target").addClass("ui-state-highlight")
+      .html("Dropped")
+      .append(ui.draggable.text());
+}
+
+function reset(event, ui) {
+  $("#target").removeClass("ui-state-highlight")
+      .html("Drop on me");
+}
+
