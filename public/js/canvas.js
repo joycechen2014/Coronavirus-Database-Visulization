446fd24016d094a08f4b147380e067fba931b6ad
wing324 <yu.min@sjsu.edu>
2020-03-20 16:34:47 -0700

    remove unused code

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -6,27 +6,6 @@ window.onload = function() {
   context.fillText("PRAY FOR WUHAN!", 25, 45);
 };
 
-// Drag and Drop Me
-//$(init);
-//function init() {
-//  $("#topPay").draggable();
-//  $("#target").droppable();
-//
-//  $("#target").bind("drop", highlightTarget);
-//  $("#target").bind("dropout", reset);
-//}
-//
-//function highlightTarget(event, ui) {
-//  $("#target").addClass("ui-state-highlight")
-//      .html("Dropped")
-//      .append(ui.draggable.text());
-//}
-//
-//function reset(event, ui) {
-//  $("#target").removeClass("ui-state-highlight")
-//      .html("Drop on me");
-//}
-
 
 $(init);
 function init() {
